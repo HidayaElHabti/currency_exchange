c445806a49605af7bd39ae32292d82d4e9ffd8a5
Hidaya El Habti <hadouya24@gmail.com>
2023-05-26 09:57:25 +0100

    vault configuration

--- src/main/java/sgabs/difa/currency_exchange/CurrencyExchangeApplication.java
+++ src/main/java/sgabs/difa/currency_exchange/CurrencyExchangeApplication.java
@@ -1,15 +1,27 @@
 package sgabs.difa.currency_exchange;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import io.micrometer.core.aop.TimedAspect;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
-public class CurrencyExchangeApplication {
+@EnableConfigurationProperties(Configuration.class)
+public class CurrencyExchangeApplication implements CommandLineRunner{
+	Logger logger = LoggerFactory.getLogger(CurrencyExchangeApplication.class);
+	private final Configuration configuration;
+
+	public CurrencyExchangeApplication(Configuration configuration){
+		this.configuration = configuration;
+		logger.info("constructor******************\n\n\n ");
+		logger.info(configuration.toString());
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyExchangeApplication.class, args);
@@ -18,6 +30,18 @@ public class CurrencyExchangeApplication {
 	@Bean
 	public TimedAspect timedAspect(MeterRegistry registry) {
 		return new TimedAspect(registry);
+	}
+
+	@Override
+	public void run(String... args) {
+
+
+
+		logger.info("----------------------------------------");
+		logger.info("Configuration properties");
+		logger.info("   example.username is {}", configuration.getUsername());
+		logger.info("   example.password is {}", configuration.getPassword());
+		logger.info("----------------------------------------");
 	}
 
 }
